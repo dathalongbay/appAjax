fa70f44a7af9d55d2006e7cc16d1aa05a69b3ab4
datdofpt <dathalongbay@gmail.com>
2021-01-03 08:28:43 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+
+    
+    $(document).ready(function () {
+
+        $("#load-more").on('click', function (event) {
+            event.preventDefault();
+
+            var params = {};
+            params.current = $(".album-list").children('.col-md-4').length;
+
+            params.limit = 3;
+
+            console.log(params);
+
+            $.ajax({
+                url:"http://localhost/appAjax/ajax_sql/ajax.php",
+                data: params,
+                type:"POST",
+                dataType:"json",
+                beforeSend:function () {
+                    alert("Trước khi gửi request bằng ajax");
+                },
+                success: function (data) {
+                    alert("ajax trả về thành công");
+                    console.log(data);
+                    $(".album-list").append(data.html);
+                },
+                error: function (xhr) {
+                    alert("Lỗi ajax");
+                },
+                complete: function (xhr, status) {
+                    alert("ajax hoàn tất");
+                }
+            });
+
+        });
+    });
